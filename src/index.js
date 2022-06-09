cb173fb36effe84f92120c60e2ba8c67d8cb53c8
Doina Colun <doinacolun@macpro.local>
2022-06-08 17:59:56 -0700

    Adding buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 'use strict';
+const totalCount = document.getElementById("tempdisplay");
+    let count = 0;
+    totalCount.innerHTML = count;
+
+const handleIncrement = () => {
+    count++;
+    totalCount.innerHTML = count;
+}
+
+const addTemperature = (event) => {
+    let newTemp = 1;
+    // const totalCount = document.getElementById("tempdisplay");
+    // let count = 0;
+    // totalCount.innerHTML = count;
+}
+
+const temperatureUpButton = (event) => {
+    const upButton = document.querySelector("#upButton");
+    upButton.addEventListener("click", handleIncrement);
+}
+
+const temperatureDownButton = (event) => {
+    const downButton = document.querySelector("#downButton");
+    downButton.addEventListener("click", addTemperature);
+}
