34885ef9dad2c6f0c87656f1f1341e861cf5b3cc
Fena Dang <fenadang@gmail.com>
2022-06-09 10:13:17 -0700

    added city header

--- src/index.js
+++ src/index.js
@@ -2,50 +2,40 @@
 // ************** Code I've added ******************************************
 
 let data = 0;
+let header = 'Weather Report';
 
-document.getElementById("root").innerText=data;
+document.getElementById('root').innerText = data;
+document.getElementById('topHeader').innerText = header;
 
-function increment(){
-    data = data + 1;
-    document.getElementById("root").innerText=data;
-    changeColor(data);  
+function increment() {
+  data = data + 1;
+  document.getElementById('root').innerText = data;
+  changeColor(data);
 }
 
-function decrement(){
-    data=data-1;
-    document.getElementById("root").innerText=data;
-    changeColor(data);
+function decrement() {
+  data = data - 1;
+  document.getElementById('root').innerText = data;
+  changeColor(data);
+}
+
+function cityname() {
+  let city = document.getElementById('cityName').value;
+  let headerWithCity = `Weather Report for City of ${city}`;
+  console.log(headerWithCity);
+  document.getElementById('topHeader').innerText = headerWithCity;
 }
 
 const changeColor = (data) => {
-    if (data <= 49) {
-        document.getElementById("root").style.color = "#008080";
-    } else if (data <= 59) {
-        document.getElementById("root").style.color = "#008000";
-    } else if (data <= 69) {
-        document.getElementById("root").style.color = "#FFFF00";
-    } else if (data <= 79) {
-        document.getElementById("root").style.color = "#FFA500";
-    } else {
-        document.getElementById("root").style.color = "#FF0000";
-    }
-}
-
-// ****************************************************************************
-
-const addTemperature = (event) => {
-    let newTemp = 1;
-    // const totalCount = document.getElementById("tempdisplay");
-    // let count = 0;
-    // totalCount.innerHTML = count;
-}
-
-const temperatureUpButton = (event) => {
-    const upButton = document.querySelector("#upButton");
-    upButton.addEventListener("click", handleIncrement);
-}
-
-const temperatureDownButton = (event) => {
-    const downButton = document.querySelector("#downButton");
-    downButton.addEventListener("click", addTemperature);
-}
+  if (data <= 49) {
+    document.getElementById('root').style.color = '#008080';
+  } else if (data <= 59) {
+    document.getElementById('root').style.color = '#008000';
+  } else if (data <= 69) {
+    document.getElementById('root').style.color = '#FFFF00';
+  } else if (data <= 79) {
+    document.getElementById('root').style.color = '#FFA500';
+  } else {
+    document.getElementById('root').style.color = '#FF0000';
+  }
+};
