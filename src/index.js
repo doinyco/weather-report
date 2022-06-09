fd44251a0ad831c4ef982436db2de53903e6b5d3
Doina Colun <doinacolun@macpro.local>
2022-06-08 20:35:19 -0700

    Added increase/ decrease button, wrote function to change color depending on the temperature + added a little bit of CSS

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 'use strict';
-const totalCount = document.getElementById("tempdisplay");
-    let count = 0;
-    totalCount.innerHTML = count;
+// ************** Code I've added ******************************************
 
-const handleIncrement = () => {
-    count++;
-    totalCount.innerHTML = count;
+let data = 0;
+
+document.getElementById("root").innerText=data;
+
+function increment(){
+    data = data + 1;
+    document.getElementById("root").innerText=data;
+    changeColor(data);  
 }
+
+function decrement(){
+    data=data-1;
+    document.getElementById("root").innerText=data;
+    changeColor(data);
+}
+
+const changeColor = (data) => {
+    if (data <= 49) {
+        document.getElementById("root").style.color = "#008080";
+    } else if (data <= 59) {
+        document.getElementById("root").style.color = "#008000";
+    } else if (data <= 69) {
+        document.getElementById("root").style.color = "#FFFF00";
+    } else if (data <= 79) {
+        document.getElementById("root").style.color = "#FFA500";
+    } else {
+        document.getElementById("root").style.color = "#FF0000";
+    }
+}
+
+// ****************************************************************************
 
 const addTemperature = (event) => {
     let newTemp = 1;
