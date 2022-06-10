6d28a77457d03028d2ba744c32d42b566fdbdc9b
Fena Dang <fenadang@gmail.com>
2022-06-09 18:09:41 -0700

    Finish API calls + changed temp/weather garden based on city

--- src/index.js
+++ src/index.js
@@ -1,32 +1,64 @@
 'use strict';
 
-let data = 32;
+let data = 55;
 let header = 'Weather Report';
 
 document.getElementById('root').innerText = data;
 document.getElementById('topHeader').innerText = header;
+
+function weatherAPICall(lat, lon) {
+  const WEATHER_API = 'http://127.0.0.1:5000/weather';
+
+  axios
+    .get(`${WEATHER_API}`, {
+      params: {
+        lat: lat,
+        lon: lon,
+      },
+    })
+    .then((response) => {
+      // console.log(response);
+      // console.log(response.data.current.temp);
+      let temp = 1.8 * (response.data.current.temp - 273.15) + 32;
+      document.getElementById('root').innerText = Math.round(temp);
+      changeColor(temp);
+      setLandscape(temp);
+      // console.log(temp);
+    })
+    .catch((response) => {
+      console.log('error in API call');
+      console.log(response);
+    });
+}
+
+function locationAPICall(city) {
+  const LOCATION_API = 'http://127.0.0.1:5000/location';
+  axios
+    .get(`${LOCATION_API}`, {
+      params: {
+        q: city,
+      },
+    })
+    .then((response) => {
+      let lat = response.data[0].lat;
+      let lon = response.data[0].lon;
+      weatherAPICall(lat, lon);
+      // console.log(lat);
+      // console.log(lon);
+    })
+    .catch((response) => {
+      console.log('error in API call');
+      console.log(response);
+    });
+}
 
 function cityname() {
   let city = document.getElementById('cityName').value;
   let headerWithCity = `Weather Report for City of ${city}`;
   console.log(headerWithCity);
   document.getElementById('topHeader').innerText = headerWithCity;
-
-  const axios = require('axios');
-  const API = 'http://127.0.0.1:5000/location';
-  axios
-    .get(`${API}`, {
-      params: {
-        q: city,
-      },
-    })
-    .then((response) => {
-      console.log(response.data[0].lat);
-    })
-    .catch((response) => {
-      console.log('error in API call');
-      console.log(response);
-    });
+  locationAPICall(city);
+  // const axios = require('axios');
 }
 
 const changeColor = (data) => {
@@ -47,19 +79,19 @@ function increment() {
   data = data + 1;
   document.getElementById('root').innerText = data;
   changeColor(data);
-  addingLandscape();
+  setLandscape(data);
 }
 
 function decrement() {
   data = data - 1;
   document.getElementById('root').innerText = data;
   changeColor(data);
-  addingLandscape();
-}
-
-function addingLandscape() {
   setLandscape(data);
 }
+
+// function addingLandscape() {
+//   setLandscape(data);
+// }
 
 const setLandscape = (data) => {
   if (data <= 59) {
